59e44c98a7519a4d507f7494f3f7f5c1afb3a3b7
MateusNeeves <mateus1603@gmail.com>
2025-05-07 21:08:54 -0300

    Adicionar tíulo SIAPRA à tela de login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,18 +15,32 @@
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <style> *{font-family: "Poppins", sans-serif;}</style>
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
         <!-- MEU CSS -->
         <link rel="stylesheet" href="./css/styles.css">
 
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <img src="{{ asset('images/logo-crcn-ne-vertical-principal.png') }}" alt="Logo" width="200" height="100">
+        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 bg-gray-100">
+            <div class="mt-6">
+                <img class="mt-6" src="{{ asset('images/logo-crcn-ne-vertical-principal.png') }}" alt="Logo" width="150" height="75">
             </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+            <br> <br><br>
+            <div class=" flex flex-col items-center sm:justify-center" style="width: 500px">
+                <h1 class="text-center" style="font-family: 'Roboto Condensed', sans-serif; font-size: 4rem; color: #f3523b; font-weight: 1000">
+                    SIAPRA
+                </h1>
+                
+                <h1 class="text-center" style="font-family: 'Roboto Condensed', sans-serif; font-size: 1.5rem; color: #f3523b; font-weight: 350;">
+                    Sistema de Apoio à Produção <br> de Radiofármacos
+                </h1>
+                
+                <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    {{ $slot }}
+                </div>
             </div>
         </div>
     </body>
